0b4c9eb762a4ad72a688afb7a13d032c64630ef8
Aniruddh Upadhyay <aniruddh622003@gmail.com>
2022-01-09 23:02:17 +0530

    Card Functionality in Progress

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,10 +8,50 @@ import {
   StatusBar,
   TouchableOpacity,
   Image,
+  StyleSheet,
 } from "react-native";
 import useAuth from "../hooks/useAuth";
 import tw from "tailwind-rn";
 import { Ionicons } from "@expo/vector-icons";
+import Swiper from "react-native-deck-swiper";
+const DUMMY_DATA = [
+  {
+    firstName: "Aniruddh",
+    lastName: "Upadhyay",
+    job: "Student",
+    age: 27,
+    photoURL:
+      "https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg",
+    key: 1,
+  },
+  {
+    firstName: "Aniruddh",
+    lastName: "Upadhyay",
+    job: "Student",
+    age: 27,
+    photoURL:
+      "https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg",
+    key: 2,
+  },
+  {
+    firstName: "Aniruddh",
+    lastName: "Upadhyay",
+    job: "Student",
+    age: 27,
+    photoURL:
+      "https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg",
+    key: 3,
+  },
+  {
+    firstName: "Aniruddh",
+    lastName: "Upadhyay",
+    job: "Student",
+    age: 27,
+    photoURL:
+      "https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg",
+    key: 4,
+  },
+];
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -22,7 +62,9 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView style={{ marginTop: StatusBar.currentHeight + 10 }}>
+    <SafeAreaView
+      style={[tw("flex-1"), { marginTop: StatusBar.currentHeight + 10 }]}
+    >
       <View style={tw("flex-row items-center justify-between relative px-5")}>
         <TouchableOpacity onPress={logout}>
           <Image
@@ -42,8 +84,103 @@ const HomeScreen = () => {
           <Ionicons name="chatbubbles-sharp" size={25} color="#ff5864" />
         </TouchableOpacity>
       </View>
+
+      <View style={tw("flex-1 -mt-6")}>
+        <Swiper
+          containerStyle={{
+            backgroundColor: "transparent",
+          }}
+          stackSize={5}
+          cardIndex={0}
+          verticalSwipe={false}
+          animateCardOpacity
+          overlayLabels={{
+            left: {
+              title: "Sorry",
+              style: {
+                label: {
+                  backgroundColor: "red",
+                  borderColor: "red",
+                  color: "white",
+                  borderWidth: 1,
+                },
+                wrapper: {
+                  flexDirection: "column",
+                  alignItems: "flex-end",
+                  justifyContent: "flex-start",
+                  marginTop: 30,
+                  marginLeft: -10,
+                },
+              },
+            },
+            right: {
+              title: "Match",
+              style: {
+                label: {
+                  backgroundColor: "green",
+                  borderColor: "green",
+                  color: "white",
+                  borderWidth: 1,
+                },
+                wrapper: {
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  justifyContent: "flex-start",
+                  marginTop: 30,
+                  marginLeft: 10,
+                },
+              },
+            },
+          }}
+          cards={DUMMY_DATA}
+          renderCard={(card) => (
+            <View
+              key={card.id}
+              style={[
+                tw("relative bg-white h-3/4 rounded-xl"),
+                styles.cardShadow,
+              ]}
+            >
+              <Image
+                style={tw("absolute top-0 h-full w-full rounded-xl")}
+                source={{ uri: card.photoURL }}
+              />
+              <View
+                style={tw(
+                  "absolute bottom-0 bg-white w-full h-20 justify-between items-center flex-row px-6 py-2 rounded-b-xl"
+                )}
+              >
+                <View>
+                  <Text style={tw("text-xl font-bold")}>
+                    {card.firstName} {card.lastName}
+                  </Text>
+                  <Text>{card.job}</Text>
+                </View>
+                <Text style={tw("text-2xl font-bold")}>{card.age}</Text>
+              </View>
+            </View>
+          )}
+          onSwiped={(cardIndex) => {
+            console.log(cardIndex);
+          }}
+          onSwipedLeft={() => console.log("Pass")}
+        />
+      </View>
     </SafeAreaView>
   );
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  cardShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+});
